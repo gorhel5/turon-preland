66bf6fe1e87ffe327b540e4f2df1c0bbe48fdbec
gorhel5 <krutilossundo@gmail.com>
2025-10-11 19:12:02 +0200

    update geo checker

--- lib/api/telegramAPI.ts
+++ lib/api/telegramAPI.ts
@@ -1,9 +1,21 @@
-import ipify from "ipify";
+// import ipify from "ipify";
+import axios from "axios";
 import { nextServer } from "./api";
+import { setCountryFlag } from "./setCountryFlag";
+
+interface UserData {
+  ip: string;
+  country: string;
+}
 
 export const sendMessage = async () => {
-  const ipRes = await ipify({ useIPv6: false });
-  const text = `<b>👁️ new page view</b>\n\n<i>🌐 ${ipRes}</i>`;
+  // const ipRes = await ipify({ useIPv6: false });
+  // const text = `<b>👁️ new page view</b>\n\n<i>🌐 ${ipRes}</i>`;
+
+  const { data } = await axios.get<UserData>("https://api.country.is");
+  const text = `<b>👁️ new page view</b>\n\n<i>🌐 ${
+    data.ip
+  }</i>\n${setCountryFlag(data.country)}`;
 
   const res = await nextServer.post("/sendmessage", {
     chat_id: process.env.NEXT_PUBLIC_CHAT_ID,
